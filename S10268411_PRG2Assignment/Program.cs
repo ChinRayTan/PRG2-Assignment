39396b4c96d5c244885bcf1dd30eeacfce582e59
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-01-15 02:07:01 +0800

    Written basic UI

--- S10268411_PRG2Assignment/Program.cs
+++ S10268411_PRG2Assignment/Program.cs
@@ -9,6 +9,63 @@
         static void Main(string[] args)
         {
             InitialiseValues();
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine();
+                Console.WriteLine("=============================================");
+                Console.WriteLine("Welcome to Changi Airport Terminal 5");
+                Console.WriteLine("=============================================");
+                Console.WriteLine("1. List all Flights");
+                Console.WriteLine("2. List Boarding Gates");
+                Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+                Console.WriteLine("4. Create Flight");
+                Console.WriteLine("5. Display Airline Flights");
+                Console.WriteLine("6. Modify flight details");
+                Console.WriteLine("7. Display Flight Schedule");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine();
+                Console.WriteLine("Please select an option: ");
+                if (Int32.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        case 5:
+                            break;
+
+                        case 6:
+                            break;
+
+                        case 7:
+                            break;
+
+                        case 0:
+                            Console.WriteLine("Goodbye!");
+                            running = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid option!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option!");
+                }
+            }
         }
 
         private static void InitialiseValues()
